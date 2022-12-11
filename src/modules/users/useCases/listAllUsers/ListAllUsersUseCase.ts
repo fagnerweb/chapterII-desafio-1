f98768f0907ce968b75dbbc6f44a23a5714cb1bd
Fagner Silva <fagnermarketing2014@gmail.com>
2022-12-11 14:02:07 -0300

    Finished project

---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -0,0 +1,29 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+    user_id: string;
+}
+
+class ListAllUsersUseCase {
+    constructor(private usersRepository: IUsersRepository) {}
+
+    execute({ user_id }: IRequest): User[] {
+        if (!user_id) {
+            throw new Error("user_id not informed in headers");
+        }
+
+        const user = this.usersRepository.findById(user_id);
+        if (!user) {
+            throw new Error("User not found");
+        }
+
+        if (!user.admin) {
+            throw new Error("User is not an admin");
+        }
+
+        return this.usersRepository.list();
+    }
+}
+
+export { ListAllUsersUseCase };
